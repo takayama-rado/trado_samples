e46cd56e472e36b18e39a404340bfff26cc22cc7
takayaman <takayaman>
2024-04-28 08:58:36 +0900

    Refactor Transformer layers.

--- src/modules_gislr/layers.py
+++ src/modules_gislr/layers.py
@@ -16,7 +16,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import copy
 import math
+from inspect import signature
 
 # Third party's modules
 import numpy as np
@@ -26,7 +28,9 @@ from torch import nn
 from torch.nn import functional as F
 
 # Local modules
-
+from .utils import (
+    make_san_mask,
+    select_reluwise_activation)
 
 # Execution settings
 VERSION = u"%(prog)s dev"
@@ -346,60 +350,349 @@ class PositionalEncoding(nn.Module):
         return feature
 
 
+class MultiheadAttention(nn.Module):
+    """Multi-headed attention (MHA) layer.
+
+    # Args:
+      - key_dim: The dimension of key.
+      - query_dim: The dimension of query.
+      - att_dim: The dimension of attention space.
+      - out_dim: The dimension of output.
+      - num_heads: The number of heads.
+      - dropout: The dropout probability for attention weights.
+      - add_bias: If True, use bias term in linear layers.
+    """
+    def __init__(self,
+                 key_dim,
+                 query_dim,
+                 att_dim,
+                 out_dim,
+                 num_heads,
+                 dropout,
+                 add_bias):
+        super().__init__()
+
+        assert att_dim % num_heads == 0
+        self.head_dim = att_dim // num_heads
+        self.num_heads = num_heads
+        self.scale = math.sqrt(self.head_dim)
+
+        self.w_key = nn.Linear(key_dim, att_dim, bias=add_bias)
+        self.w_value = nn.Linear(key_dim, att_dim, bias=add_bias)
+        self.w_query = nn.Linear(query_dim, att_dim, bias=add_bias)
+
+        self.w_out = nn.Linear(att_dim, out_dim, bias=add_bias)
+
+        self.dropout_attn = nn.Dropout(p=dropout)
+
+        self.neg_inf = None
+
+    def reset_parameters(self, add_bias):
+        """Initialize parameters with Xavier uniform distribution.
+
+        # NOTE: For this initialization, please refer
+        https://github.com/pytorch/fairseq/blob/master/fairseq/modules/multihead_attention.py  # pylint: disable=line-too-long
+
+        """
+        nn.init.xavier_uniform_(self.w_key.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.w_value.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.w_query.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.w_out.weight)
+        if add_bias:
+            nn.init.constant_(self.w_key.bias, 0.)
+            nn.init.constant_(self.w_value.bias, 0.)
+            nn.init.constant_(self.w_query.bias, 0.)
+            nn.init.constant_(self.w_out.bias, 0.)
+
+    def forward(self,
+                key: torch.Tensor,
+                value: torch.Tensor,
+                query: torch.Tensor,
+                mask: torch.Tensor):
+        """Perform forward computation.
+
+        # Args:
+          - key: `[N, klen, key_dim]`
+          - value: `[N, klen, vdim]`
+          - query: `[N, qlen, query_dim]`
+          - mask: `[N, qlen, klen]`
+        # Returns:
+          - cvec: The context vector. `[N, qlen, vdim]`
+          - aws: The attention weights. `[N, H, qlen, klen]`
+        """
+        if self.neg_inf is None:
+            self.neg_inf = float(np.finfo(
+                torch.tensor(0, dtype=key.dtype).numpy().dtype).min)
+
+        bsize, klen = key.size()[: 2]
+        qlen = query.size(1)
+
+        # key: `[N, klen, kdim] -> [N, klen, adim] -> [N, klen, H, adim/H(=hdim)]`
+        # value: `[N, klen, vdim] -> [N, klen, adim] -> [N, klen, H, adim/H(=hdim)]`
+        # query: `[N, qlen, qdim] -> [N, qlen, adim] -> [N, qlen, H, adim/H(=hdim)]`
+        key = self.w_key(key).reshape([bsize, -1, self.num_heads, self.head_dim])
+        value = self.w_value(value).reshape([bsize, -1, self.num_heads, self.head_dim])
+        query = self.w_query(query).reshape([bsize, -1, self.num_heads, self.head_dim])
+
+        # qk_score: `[N, qlen, H, hdim] x [N, klen, H, hdim] -> [N, qlen, klen, H]`
+        qk_score = torch.einsum("bihd,bjhd->bijh", (query, key)) / self.scale
+
+        # Apply mask.
+        if mask is not None:
+            # `[N, qlen, klen] -> [N, qlen, klen, H]`
+            mask = mask.unsqueeze(3).repeat([1, 1, 1, self.num_heads])
+            mask_size = (bsize, qlen, klen, self.num_heads)
+            assert mask.size() == mask_size, f"{mask.size()}:{mask_size}"
+            # Negative infinity should be 0 in softmax.
+            qk_score = qk_score.masked_fill_(mask == 0, self.neg_inf)
+        # Compute attention weight.
+        attw = torch.softmax(qk_score, dim=2)
+        attw = self.dropout_attn(attw)
+
+        # cvec: `[N, qlen, klen, H] x [N, qlen, h, hdim] -> [N, qlen, H, hdim]
+        # -> [N, qlen, H * hdim]`
+        cvec = torch.einsum("bijh,bjhd->bihd", (attw, value))
+        cvec = cvec.reshape([bsize, -1, self.num_heads * self.head_dim])
+        cvec = self.w_out(cvec)
+        # attw: `[N, qlen, klen, H]` -> `[N, H, qlen, klen]`
+        attw = attw.permute(0, 3, 1, 2)
+        return cvec, attw
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self,
+                 dim_model,
+                 dim_ffw,
+                 dropout,
+                 activation,
+                 add_bias):
+       super().__init__()
+
+       self.w_1 = nn.Linear(dim_model, dim_ffw, bias=add_bias)
+       self.w_2 = nn.Linear(dim_ffw, dim_model, bias=add_bias)
+
+       self.dropout = nn.Dropout(p=dropout)
+
+       self.activation = select_reluwise_activation(activation)
+
+    def forward(self, feature):
+        feature = self.w_1(feature)
+        feature = self.activation(feature)
+        feature = self.dropout(feature)
+        feature = self.w_2(feature)
+        return feature
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self,
+                 dim_model,
+                 num_heads,
+                 dim_ffw,
+                 dropout,
+                 activation,
+                 layer_norm_eps,
+                 norm_first,
+                 add_bias):
+        super().__init__()
+
+        self.norm_first = norm_first
+
+        self.self_attn = MultiheadAttention(
+            key_dim=dim_model,
+            query_dim=dim_model,
+            att_dim=dim_model,
+            out_dim=dim_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            add_bias=add_bias)
+
+        self.ffw = PositionwiseFeedForward(
+            dim_model=dim_model,
+            dim_ffw=dim_ffw,
+            dropout=dropout,
+            activation=activation,
+            add_bias=add_bias)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        # The argument `bias` was added at v2.1.0.
+        # So, we check whether LayerNorm has this.
+        sig = signature(nn.LayerNorm)
+        if "bias" in sig.parameters:
+            self.norm1 = nn.LayerNorm(dim_model, eps=layer_norm_eps, bias=add_bias)
+            self.norm2 = nn.LayerNorm(dim_model, eps=layer_norm_eps, bias=add_bias)
+        else:
+            self.norm1 = nn.LayerNorm(dim_model, eps=layer_norm_eps)
+            self.norm2 = nn.LayerNorm(dim_model, eps=layer_norm_eps)
+
+        # To store attention weights.
+        self.attw = None
+
+    def _forward_prenorm(self,
+                         feature,
+                         san_mask):
+        """Pre-normalization structure.
+
+        For the details, please refer
+        https://arxiv.org/pdf/2002.04745v1.pdf
+        """
+        #################################################
+        # self-attention
+        #################################################
+        # `[N, qlen, dim_model]`
+        residual = feature
+        feature = self.norm1(feature)
+        feature, self.attw = self.self_attn(
+            key=feature,
+            value=feature,
+            query=feature,
+            mask=san_mask)
+        feature = self.dropout(feature) + residual
+
+        #################################################
+        # FFW
+        #################################################
+        residual = feature
+        # `[N, qlen, dim_model]`
+        feature = self.norm2(feature)
+        feature = self.ffw(feature)
+        feature = self.dropout(feature) + residual
+        return feature
+
+    def _forward_postnorm(self,
+                          feature,
+                          san_mask):
+        """Post-normalization structure (standard).
+
+        """
+        #################################################
+        # self-attention
+        #################################################
+        # `[N, qlen, dim_model]`
+        residual = feature
+        feature, self.attw = self.self_attn(
+            key=feature,
+            value=feature,
+            query=feature,
+            mask=san_mask)
+        feature = self.dropout(feature) + residual
+        feature = self.norm1(feature)
+
+        #################################################
+        # FFW
+        #################################################
+        residual = feature
+        # `[N, qlen, dim_model]`
+        feature = self.ffw(feature)
+        feature = self.dropout(feature) + residual
+        feature = self.norm2(feature)
+        return feature
+
+    def forward(self,
+                feature,
+                causal_mask=None,
+                src_key_padding_mask=None):
+        bsize, qlen = feature.shape[:2]
+        if src_key_padding_mask is not None:
+            san_mask = make_san_mask(src_key_padding_mask, causal_mask)
+        elif causal_mask is not None:
+            san_mask = causal_mask
+        else:
+            san_mask = None
+
+        if self.norm_first:
+            feature = self._forward_prenorm(feature, san_mask)
+        else:
+            feature = self._forward_postnorm(feature, san_mask)
+
+        return feature
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self,
+                 encoder_layer,
+                 num_layers,
+                 dim_model,
+                 dropout_pe,
+                 layer_norm_eps,
+                 norm_first,
+                 add_bias,
+                 add_tailnorm):
+        super().__init__()
+
+        self.pos_encoder = PositionalEncoding(dim_model, dropout_pe)
+        self.layers = nn.ModuleList([copy.deepcopy(encoder_layer) for _ in range(num_layers)])
+
+        # Add LayerNorm at tail position.
+        # This is applied only when norm_first is True because
+        # post-normalization structure includes tail-normalization in encoder
+        # layers.
+        if add_tailnorm and norm_first:
+            # The argument `bias` was added at v2.1.0.
+            # So, we check whether LayerNorm has this.
+            sig = signature(nn.LayerNorm)
+            if "bias" in sig.parameters:
+                self.norm = nn.LayerNorm(dim_model, eps=layer_norm_eps, bias=add_bias)
+            else:
+                self.norm = nn.LayerNorm(dim_model, eps=layer_norm_eps)
+        else:
+            self.norm = Identity()
+
+    def forward(self,
+                feature,
+                causal_mask,
+                src_key_padding_mask):
+        feature = self.pos_encoder(feature)
+        for layer in self.layers:
+            feature = layer(feature,
+                            causal_mask,
+                            src_key_padding_mask)
+        feature = self.norm(feature)
+        return feature
+
+
 class TransformerEnISLR(nn.Module):
     def __init__(self,
                  in_channels,
                  inter_channels,
                  out_channels,
-                 num_head=1,
-                 num_layers=1,
-                 dim_feedforward=256,
-                 batch_first=True,
-                 norm_first=True,
-                 dropoutrate=0.1):
+                 activation="relu",
+                 tren_num_layers=1,
+                 tren_num_heads=1,
+                 tren_dim_ffw=256,
+                 tren_dropout_pe=0.1,
+                 tren_dropout=0.1,
+                 tren_layer_norm_eps=1e-5,
+                 tren_norm_first=True,
+                 tren_add_bias=True,
+                 tren_add_tailnorm=True):
         super().__init__()
 
-        self._build_feature_extractor(in_channels, inter_channels)
-
-        self._build_tr_encoder(inter_channels, num_head, num_layers,
-                               dim_feedforward, dropoutrate,
-                               batch_first, norm_first)
-
-        self._build_recognition_head(inter_channels, out_channels)
-
-    def _build_feature_extractor(self, in_channels, inter_channels):
+        # Feature extraction.
         self.linear = nn.Linear(in_channels, inter_channels)
-        self.activation = nn.ReLU()
+        self.activation = select_reluwise_activation(activation)
 
-    def _build_tr_encoder(self,
-                          inter_channels,
-                          nhead,
-                          num_layers,
-                          dim_feedforward,
-                          dropoutrate,
-                          batch_first,
-                          norm_first):
-        self.pe = PositionalEncoding(dim_model=inter_channels,
-                                     dropout=dropoutrate)
-        enlayer = nn.TransformerEncoderLayer(
-            d_model=inter_channels,
-            nhead=nhead,
-            dim_feedforward=dim_feedforward,
-            dropout=dropoutrate,
-            batch_first=batch_first,
-            norm_first=norm_first)
-        self.tr_encoder = nn.TransformerEncoder(
+        # Transformer-Encoder.
+        enlayer = TransformerEncoderLayer(
+                dim_model=inter_channels,
+                num_heads=tren_num_heads,
+                dim_ffw=tren_dim_ffw,
+                dropout=tren_dropout,
+                activation=activation,
+                layer_norm_eps=tren_layer_norm_eps,
+                norm_first=tren_norm_first,
+                add_bias=tren_add_bias)
+        self.tr_encoder = TransformerEncoder(
             encoder_layer=enlayer,
-            num_layers=num_layers)
+            num_layers=tren_num_layers,
+            dim_model=inter_channels,
+            dropout_pe=tren_dropout_pe,
+            layer_norm_eps=tren_layer_norm_eps,
+            norm_first=tren_norm_first,
+            add_bias=tren_add_bias,
+            add_tailnorm=tren_add_tailnorm)
 
-    def _build_recognition_head(self,
-                                inter_channels,
-                                out_channels):
         self.head = GPoolRecognitionHead(inter_channels, out_channels)
-
-    def _invert_mask(self, mask):
-        _mask = torch.logical_not(mask) if mask is not None else None
-        return _mask
 
     def forward(self,
                 feature,
@@ -418,26 +711,15 @@ class TransformerEnISLR(nn.Module):
         if torch.isnan(feature).any():
             raise ValueError()
 
-        # Apply PE.
-        feature = self.pe(feature)
-        if torch.isnan(feature).any():
-            raise ValueError()
-
-        # Note:
-        # Different from the other layers, the attention of pytorch's
-        # Transformer attends to `False` position indicated by the input masks.
-        # Therefor, we invert the flags of the input masks here.
-        _feature_causal_mask = self._invert_mask(feature_causal_mask)
-        _feature_pad_mask = self._invert_mask(feature_pad_mask)
-
-        feature = self.tr_encoder(src=feature,
-            mask=_feature_causal_mask,
-            src_key_padding_mask=_feature_pad_mask)
+        feature = self.tr_encoder(
+            feature=feature,
+            causal_mask=feature_causal_mask,
+            src_key_padding_mask=feature_pad_mask)
         if torch.isnan(feature).any():
             raise ValueError()
 
         # `[N, T, C] -> [N, C, T]`
-        logit = self.head(feature.permute([0, 2, 1]))
+        logit = self.head(feature.permute([0, 2, 1]), feature_pad_mask)
         if torch.isnan(feature).any():
             raise ValueError()
         return logit
