e458c75462a407294fcc6b3d92aa60de1d3228bd
takayaman <takayaman>
2024-08-05 21:58:10 +0900

    Add MacarronNet.

--- src/modules_gislr/layers.py
+++ src/modules_gislr/layers.py
@@ -781,6 +781,209 @@ class TransformerEnISLR(nn.Module):
         return logit
 
 
+class MacarronNetEncoderLayer(nn.Module):
+    def __init__(self,
+                 dim_model,
+                 num_heads,
+                 dim_ffw,
+                 dropout,
+                 activation,
+                 norm_type_sattn,
+                 norm_type_ffw,
+                 norm_eps,
+                 add_bias):
+        super().__init__()
+
+        self.fc_factor = 0.5
+
+        # =====================================================================
+        # First half PFFN.
+        # =====================================================================
+        self.norm_ffw1 = create_norm(norm_type_ffw, dim_model, norm_eps, add_bias)
+        self.ffw1 = PositionwiseFeedForward(
+            dim_model=dim_model,
+            dim_ffw=dim_ffw,
+            dropout=dropout,
+            activation=activation,
+            add_bias=add_bias)
+        # =====================================================================
+        # MHA.
+        # =====================================================================
+        self.norm_sattn = create_norm(norm_type_sattn, dim_model, norm_eps, add_bias)
+        self.self_attn = MultiheadAttention(
+            key_dim=dim_model,
+            query_dim=dim_model,
+            att_dim=dim_model,
+            out_dim=dim_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            add_bias=add_bias)
+        # =====================================================================
+        # Second half PFFN.
+        # =====================================================================
+        self.norm_ffw2 = create_norm(norm_type_ffw, dim_model, norm_eps, add_bias)
+        self.ffw2 = PositionwiseFeedForward(
+            dim_model=dim_model,
+            dim_ffw=dim_ffw,
+            dropout=dropout,
+            activation=activation,
+            add_bias=add_bias)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        # To store attention weights.
+        self.attw = None
+
+    def forward(self,
+                feature,
+                causal_mask=None,
+                src_key_padding_mask=None):
+        bsize, qlen = feature.shape[:2]
+        if src_key_padding_mask is not None:
+            san_mask = make_san_mask(src_key_padding_mask, causal_mask)
+        elif causal_mask is not None:
+            san_mask = causal_mask
+        else:
+            san_mask = None
+
+        #################################################
+        # First half PFFN.
+        #################################################
+        # `[N, qlen, dim_model]`
+        residual = feature
+        feature = apply_norm(self.norm_ffw1, feature)
+        feature = self.ffw1(feature)
+        feature = self.fc_factor * self.dropout(feature) + residual
+
+        #################################################
+        # MHA.
+        #################################################
+        # `[N, qlen, dim_model]`
+        residual = feature
+        feature = apply_norm(self.norm_sattn, feature)
+        feature, self.attw = self.self_attn(
+            key=feature,
+            value=feature,
+            query=feature,
+            mask=san_mask)
+        feature = self.dropout(feature) + residual
+
+        #################################################
+        # Second half PFFN.
+        #################################################
+        # `[N, qlen, dim_model]`
+        residual = feature
+        feature = apply_norm(self.norm_ffw2, feature)
+        feature = self.ffw2(feature)
+        feature = self.fc_factor * self.dropout(feature) + residual
+
+        return feature
+
+
+class MacarronNetEnISLR(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 inter_channels,
+                 out_channels,
+                 activation="relu",
+                 pooling_type="none",
+                 tren_num_layers=1,
+                 tren_num_heads=1,
+                 tren_dim_ffw=256,
+                 tren_dropout_pe=0.1,
+                 tren_dropout=0.1,
+                 tren_norm_type_sattn="layer",
+                 tren_norm_type_ffw="layer",
+                 tren_norm_type_tail="layer",
+                 tren_norm_eps=1e-5,
+                 tren_add_bias=True,
+                 tren_add_tailnorm=True):
+        super().__init__()
+
+        # Feature extraction.
+        self.linear = nn.Linear(in_channels, inter_channels)
+        self.activation = select_reluwise_activation(activation)
+
+        if pooling_type == "none":
+            self.pooling = Identity()
+        elif pooling_type == "average":
+            self.pooling = nn.AvgPool2d(
+                kernel_size=[2, 1],
+                stride=[2, 1],
+                padding=0)
+        elif pooling_type == "max":
+            self.pooling = nn.MaxPool2d(
+                kernel_size=[2, 1],
+                stride=[2, 1],
+                padding=0)
+
+        # Transformer-Encoder.
+        enlayer = MacarronNetEncoderLayer(
+            dim_model=inter_channels,
+            num_heads=tren_num_heads,
+            dim_ffw=tren_dim_ffw,
+            dropout=tren_dropout,
+            activation=activation,
+            norm_type_sattn=tren_norm_type_sattn,
+            norm_type_ffw=tren_norm_type_ffw,
+            norm_eps=tren_norm_eps,
+            add_bias=tren_add_bias)
+        self.tr_encoder = TransformerEncoder(
+            encoder_layer=enlayer,
+            num_layers=tren_num_layers,
+            dim_model=inter_channels,
+            dropout_pe=tren_dropout_pe,
+            norm_type_tail=tren_norm_type_tail,
+            norm_eps=tren_norm_eps,
+            norm_first=True,  # Fixed for MacarronNet.
+            add_bias=tren_add_bias,
+            add_tailnorm=tren_add_tailnorm)
+
+        self.head = GPoolRecognitionHead(inter_channels, out_channels)
+
+    def forward(self,
+                feature,
+                feature_causal_mask=None,
+                feature_pad_mask=None):
+        # Feature extraction.
+        # `[N, C, T, J] -> [N, T, C, J] -> [N, T, C*J] -> [N, T, C']`
+        N, C, T, J = feature.shape
+        feature = feature.permute([0, 2, 1, 3])
+        feature = feature.reshape(N, T, -1)
+
+        feature = self.linear(feature)
+        if torch.isnan(feature).any():
+            raise ValueError()
+        feature = self.activation(feature)
+        if torch.isnan(feature).any():
+            raise ValueError()
+
+        # Apply pooling.
+        feature = self.pooling(feature)
+        if feature_pad_mask is not None:
+            # Cast to apply pooling.
+            feature_pad_mask = feature_pad_mask.to(feature.dtype)
+            feature_pad_mask = self.pooling(feature_pad_mask.unsqueeze(-1)).squeeze(-1)
+            # Binarization.
+            # This removes averaged signals with padding.
+            feature_pad_mask = feature_pad_mask > 0.5
+            if feature_causal_mask is not None:
+                feature_causal_mask = make_causal_mask(feature_pad_mask)
+
+        feature = self.tr_encoder(
+            feature=feature,
+            causal_mask=feature_causal_mask,
+            src_key_padding_mask=feature_pad_mask)
+        if torch.isnan(feature).any():
+            raise ValueError()
+
+        # `[N, T, C] -> [N, C, T]`
+        logit = self.head(feature.permute([0, 2, 1]), feature_pad_mask)
+        if torch.isnan(feature).any():
+            raise ValueError()
+        return logit
+
+
 # --- Execution --------------------------------------------------------
 if __name__ == "__main__":
     print(__doc__)
